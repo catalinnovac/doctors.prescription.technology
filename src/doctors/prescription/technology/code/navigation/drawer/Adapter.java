1c14e6dc98cedc965511cae7014f7d6e35df0b23
catalinnovac <novaccatalin90@hotmail.com>
2014-08-12 17:15:41 +0300

    third commit

---
+++ src/doctors/prescription/technology/code/navigation/drawer/Adapter.java
@@ -0,0 +1,63 @@
+package doctors.prescription.technology.code.navigation.drawer;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import doctors.prescription.technology.R;
+import doctors.prescription.technology.code.PrescriptionTechnologyWithNavigationDrawer;
+
+import java.util.List;
+
+/**
+ * Created by novac on 26-Jul-14.
+ */
+public class Adapter extends ArrayAdapter<Item> {
+
+    private static final String TAG = "ADAPTER";
+    private List<Item> internal_items;
+    private PrescriptionTechnologyWithNavigationDrawer internal_context;
+
+    public Adapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+
+    public Adapter(Context context, int resource, List<Item> items) {
+        super(context, resource, items);
+        internal_items = items;
+        internal_context = (PrescriptionTechnologyWithNavigationDrawer) context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        Item p = getItem(position);
+        if (v == null) {
+            LayoutInflater vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.left_drawer_item, null);
+        }
+        if (p != null) {
+            CustomCordovaWebView cordovaWebView = (CustomCordovaWebView) v.findViewById(R.id.cordova_left_item_webview);
+            if (cordovaWebView != null) {
+                Log.v(TAG, "LOAD URL:" + p.Id);
+                cordovaWebView.getSettings().setJavaScriptEnabled(true);
+                cordovaWebView.loadUrl(p.CONTENT);
+                internal_context.AddNavigationDrawerView("CART", cordovaWebView);
+            }
+        }
+        return v;
+
+    }
+
+    public View getViewById(String Id) {
+        int position = 0;
+        for (int i = 0; i < getCount(); i++) {
+            if (getItem(i).Id == Id)
+                position = i;
+            break;
+        }
+        return getView(position, null, null);
+    }
+}
---
+++ src/doctors/prescription/technology/code/navigation/drawer/Item.java
@@ -0,0 +1,9 @@
+package doctors.prescription.technology.code.navigation.drawer;
+
+/**
+ * Created by novac on 26-Jul-14.
+ */
+public class Item {
+    public String Id;
+    public String CONTENT;
+}
